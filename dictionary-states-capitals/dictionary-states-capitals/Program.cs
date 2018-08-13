b1620aad86d71bd64be6b79156422eb199ac002a
DaigneaultPearce <daigneault.pardopearce.515@my.csun.edu>
2018-08-12 18:20:31 -0700

    Dictionary practice using class

---
+++ dictionary-states-capitals/dictionary-states-capitals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary_states_capitals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, string> d_state_cap = new Dictionary<string, string>();
+            //Console.WriteLine("Welcome to my collection of states and capitals");
+            //d_state_cap.Add("Alabama", "Montgomery");
+            //d_state_cap.Add("Alasky", "Juneau");
+            //d_state_cap.Add("Arizona", "Phoenix");
+            //d_state_cap.Add("Massachusetts", "Boston");
+            //d_state_cap.Add("Wyoming", "Cheyenne");
+
+            var states = State.GetStates();
+            var theState = states["Alaska"];
+            Console.WriteLine("Capital of Alaska is {0}, population {1}, size {2} square miles", theState.Capital, theState.Population, theState.Size);
+        }
+
+    }
+}
---
+++ dictionary-states-capitals/dictionary-states-capitals/State.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dictionary_states_capitals
+{
+    public class State
+    {
+        public string Capital { get; set; }//state capital
+        public int Population { get; set; }//in millions
+        public int Size { get; set; }//size in square miles
+
+
+        //constructor
+        public State(string capital, int population, int size)
+        {
+            Capital = capital;
+            Population = population;
+            Size = size;
+        }
+
+        public static Dictionary<string,State> GetStates()
+        {
+            var states = new Dictionary<string, State>();
+            var theState = new State("Montgomery", 12345, 123);
+            states.Add("Alabama", theState);
+            theState = new State("juneau", 6789, 456);
+            states.Add("Alaska", theState);
+            return states;
+        }
+    }
+}
